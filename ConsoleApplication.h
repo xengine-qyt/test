2b7878292b25faf7062ed5075715e52eba3a6962
qyt <486179@qq.com>
2025-01-08 17:58:15 +0800

    3

---
+++ ConsoleApplication.h
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavdevice/avdevice.h>
+#include <libavfilter/avfilter.h>
+#include <libavfilter/buffersrc.h>
+#include <libavfilter/buffersink.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+#include <libavutil/opt.h>
+#include <libavutil/channel_layout.h>	
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+}
+
+extern "C" int video_convert();
--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,24 +1,7 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+﻿#include "ConsoleApplication.h"
 
-extern "C"
+extern "C" int video_convert()
 {
-#include <libavcodec/avcodec.h>
-#include <libavdevice/avdevice.h>
-#include <libavfilter/avfilter.h>
-#include <libavfilter/buffersrc.h>
-#include <libavfilter/buffersink.h>
-#include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/opt.h>
-#include <libavutil/channel_layout.h>	
-#include <libswresample/swresample.h>
-#include <libswscale/swscale.h>
-}
-
-int main(int argc, char** argv) {
 	AVCodecContext* codec_context = NULL;
 	const AVCodec* codec = NULL;
 	AVFormatContext* format_context = NULL;
